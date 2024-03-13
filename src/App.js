5534957bab01715fb75f90c5ead0df7d123f91bb
Malek Kazal <ghazal.malek@gmail.com>
2024-03-13 15:49:12 -0400

    image-slider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Accordian from "./components/accordian";
+import ImageSlider from "./components/image-slider";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 
@@ -14,6 +15,13 @@ function App() {
 
       {/* Star Rating Comp. */}
       <StarRating />
+
+      {/* Image Slider */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"4"}
+      />
     </div>
   );
 }
